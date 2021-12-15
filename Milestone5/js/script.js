f39919e44ed1afeeef2370341ee6af5f2e92760c
XanderGale <alessandro.galeazzi@icloud.com>
2021-12-15 17:20:36 +0100

    fixed delete message

--- Milestone5/js/script.js
+++ Milestone5/js/script.js
@@ -19,19 +19,15 @@ const boolzapp_chat = new Vue({
                         date: '10/01/2020 15:30:55',
                         text: 'Hai portato a spasso il cane?',
                         status: 'sent',
-                        visibleMessage: true,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Ricordati di dargli da mangiare',
-                        status: 'sent',
-                        visibleMessage: true,
-                    },
+                        status: 'sent',                    },
                     {
                         date: '10/01/2020 16:15:22',
                         text: 'Tutto fatto!',
                         status: 'received',
-                        visibleMessage: true,
                     }
                 ],
             },
@@ -44,19 +40,16 @@ const boolzapp_chat = new Vue({
                         date: '20/03/2020 16:30:00',
                         text: 'Ciao come stai?',
                         status: 'sent',
-                        visibleMessage: true,
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         text: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received',
-                        visibleMessage: true,
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         text: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent',
-                        visibleMessage: true,
                     }
                 ],
             },
@@ -69,19 +62,16 @@ const boolzapp_chat = new Vue({
                         date: '28/03/2020 10:10:40',
                         text: 'La Marianna va in campagna',
                         status: 'received',
-                        visibleMessage: true,
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         text: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent',
-                        visibleMessage: true,
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         text: 'Ah scusa!',
                         status: 'received',
-                        visibleMessage: true,
                     }
                 ],
             },
@@ -94,13 +84,11 @@ const boolzapp_chat = new Vue({
                         date: '10/01/2020 15:30:55',
                         text: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent',
-                        visibleMessage: true,
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         text: 'Si, ma preferirei andare al cinema',
                         status: 'received',
-                        visibleMessage: true,
                     }
                 ],
             },
@@ -150,6 +138,9 @@ const boolzapp_chat = new Vue({
             } else {
                 this.activeMessage = index;
             }
+        },
+        deleteMessage: function(index){
+            this.contacts[this.activeContact].messages.splice(index, 1);
         }
     },
 })
